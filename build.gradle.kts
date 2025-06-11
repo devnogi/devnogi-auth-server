8d15b2579b6e692ed0bb11eac09b13bc306755e2
sh.lee <sh.lee@promptech.co.kr>
2025-06-11 12:52:26 +0900

    infra: docker 수동 조작 설정 및 health check 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
 plugins {
 	id("org.springframework.boot") version "3.1.3"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.9.10"
-	kotlin("plugin.spring") version "1.9.10"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "until.the.eternity"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
 	mavenCentral()
+}
+
+kotlin {
+	jvmToolchain(21)
 }
 
 dependencies {
