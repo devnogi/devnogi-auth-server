aff831497af6168ef476a547d13d64fa88805fb6
Sanghyun Yi <njsharry32@gmail.com>
2025-06-21 16:16:51 +0900

    feat: 전역예외처리기 추가

--- src/main/java/until/the/eternity/das/common/exception/GlobalExceptionCode.java
+++ src/main/java/until/the/eternity/das/common/exception/GlobalExceptionCode.java
@@ -1,4 +1,4 @@
-package until.the.eternity.common.exception;
+package until.the.eternity.das.common.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
--- src/main/java/until/the/eternity/das/common/exception/GlobalExceptionHandler.java
+++ src/main/java/until/the/eternity/das/common/exception/GlobalExceptionHandler.java
@@ -1,13 +1,13 @@
-package until.the.eternity.common.exception;
+package until.the.eternity.das.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import until.the.eternity.dcs.common.response.ApiResponse;
+import until.the.eternity.das.common.response.ApiResponse;
 
-import static until.the.eternity.dcs.common.exception.GlobalExceptionCode.SERVER_ERROR;
+import static until.the.eternity.das.common.exception.GlobalExceptionCode.SERVER_ERROR;
 
 @Slf4j
 @RestControllerAdvice
