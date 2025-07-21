0c815b799c049df03a04576cccfa1e8873caefc4
안나 <dksek3050@gmail.com>
2025-07-21 23:15:06 +0900

    database: role 테이블에 기본 데이터 삽입

---
+++ src/main/resources/db/migration/V20250721224045__insert_default_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
+VALUES ('1', 'SUPER ADMIN', '최고 권한 관리자');
+INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
+VALUES ('2', 'ADMIN', '관리자');
+INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
+VALUES ('3', 'USER', '사용자');
