a21a64cf638657c10aad78a8352cf3636d820ba0
안나 <dksek3050@gmail.com>
2025-07-21 23:43:33 +0900

    refactor: 예외처리 문구 변경

--- src/main/java/until/the/eternity/das/auth/exception/AuthExceptionCode.java
+++ src/main/java/until/the/eternity/das/auth/exception/AuthExceptionCode.java
@@ -13,9 +13,9 @@ public enum AuthExceptionCode implements ExceptionCode {
   NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
   INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "유효하지 않은 이메일 형식입니다."),
   INVALID_NICKNAME_FORMAT(HttpStatus.BAD_REQUEST,
-      "유효하지 않은 닉네임 형식입니다. 닉네임은 2자 이상 20자 이하의 영문, 숫자, 한글로 구성되어야 합니다."),
+      "유효하지 않은 닉네임 형식입니다. 닉네임은 2자 이상 20자 이하이면서 영문, 숫자, 한글만 사용 가능합니다."),
   INVALID_PASSWORD_FORMAT(HttpStatus.BAD_REQUEST,
-      "유효하지 않은 비밀번호 형식입니다. 비밀번호는 8자 이상 20자 이하의 영문, 숫자, 특수문자로 구성되어야 합니다.");
+      "유효하지 않은 비밀번호 형식입니다. 비밀번호는 8자 이상 20자 이하이면서 1개 이상의 알파벳, 숫자, 특수문자로 구성되어야 합니다.");
 
 
   private final HttpStatus status;
