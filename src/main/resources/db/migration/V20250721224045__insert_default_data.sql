29656e7be6391b12fbd757ca16dccd641f545f4d
장욱 <janguk95@naver.com>
2025-09-07 17:46:56 +0900

    refactor: 타입명 통일

--- src/main/resources/db/migration/V20250721224045__insert_default_data.sql
+++ src/main/resources/db/migration/V20250721224045__insert_default_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
-VALUES ('1', 'SUPER ADMIN', '최고 권한 관리자');
+VALUES ('1', 'SUPER_ADMIN', '최고 권한 관리자');
 INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
 VALUES ('2', 'ADMIN', '관리자');
 INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
