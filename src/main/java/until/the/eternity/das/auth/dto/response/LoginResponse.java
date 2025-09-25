099c3853e249bf514cf1b34efab711b456dc9f02
장욱 <janguk95@naver.com>
2025-09-26 04:27:49 +0900

    feat: 로그인 응답 DTO생성

---
+++ src/main/java/until/the/eternity/das/auth/dto/response/LoginResponse.java
@@ -0,0 +1,16 @@
+package until.the.eternity.das.auth.dto.response;
+
+import until.the.eternity.das.user.entity.User;
+
+public record LoginResponse(
+  Long userId,
+  String nickname,
+  String email
+) {
+  public static LoginResponse from(User user) {
+    return new LoginResponse(
+      user.getId(),
+      user.getNickname(),
+      user.getEmail());
+  }
+}
