f4b159f51b9b6474ef92bf75ffa68bae947e6281
장욱 <janguk95@naver.com>
2025-10-09 16:35:41 +0900

    feat: kafka 이벤트 종류 상수 구현

---
+++ src/main/java/until/the/eternity/das/common/constant/UserUpdateConstant.java
@@ -0,0 +1,6 @@
+package until.the.eternity.das.common.constant;
+
+public class UserUpdateConstant {
+  public static final String INFO = "USER_INFO_UPDATE_EVENT";
+  public static final String PASSWORD = "PASSWORD_UPDATE_EVENT";
+}
