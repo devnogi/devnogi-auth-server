ff6885c67c85d377b6d906b9f2141b45286e9cda
장욱 <janguk95@naver.com>
2025-12-08 19:56:33 +0900

    feat: 비동기처리 가능하게 어노테이션 추가

--- src/main/java/until/the/eternity/das/DasApplication.java
+++ src/main/java/until/the/eternity/das/DasApplication.java
@@ -2,12 +2,14 @@ package until.the.eternity.das;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class DasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DasApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DasApplication.class, args);
+  }
 
 }
