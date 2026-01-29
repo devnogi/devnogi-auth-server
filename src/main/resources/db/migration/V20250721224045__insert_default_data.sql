f9c98fe120c57fb388a21ac90bf1c5ed53940211
장욱 <janguk95@naver.com>
2026-01-30 00:18:14 +0900

    fix: 배포 관련 로직 수정

--- src/main/resources/db/migration/V20250721224045__insert_default_data.sql
+++ src/main/resources/db/migration/V20250721224045__insert_default_data.sql
@@ -1,6 +1,6 @@
-INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
+INSERT INTO `auth`.`roles` (`id`, `name`, `description`)
 VALUES ('1', 'SUPER_ADMIN', '최고 권한 관리자');
-INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
+INSERT INTO `auth`.`roles` (`id`, `name`, `description`)
 VALUES ('2', 'ADMIN', '관리자');
-INSERT INTO `devnogi`.`roles` (`id`, `name`, `description`)
+INSERT INTO `auth`.`roles` (`id`, `name`, `description`)
 VALUES ('3', 'USER', '사용자');
