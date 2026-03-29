e31798ec5d949cb6c7964eb7200dbcef9e8d0836
장욱 <janguk95@naver.com>
2026-03-29 21:37:42 +0900

    fix: 직업 이름 사이의 공백 제거

--- src/main/java/until/the/eternity/das/user/entity/enums/NicknameWord.java
+++ src/main/java/until/the/eternity/das/user/entity/enums/NicknameWord.java
@@ -1,10 +1,11 @@
 package until.the.eternity.das.user.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
@@ -13,14 +14,14 @@ public enum NicknameWord {
   HUMAN("인간"), ELF("요정"), GIANT("자이언트"),
 
   // Jobs
-  WARRIOR("전사"), WIZARD("마법사"), ARCHER("궁수"), BATTLE_ALCHEMIST("전투 연금술사"),
+  WARRIOR("전사"), WIZARD("마법사"), ARCHER("궁수"), BATTLE_ALCHEMIST("전투연금술사"),
   FIGHTER("격투가"), BARD("음유시인"), PUPPETEER("인형사"), LANCER("랜서"),
-  ALCHEMY_MASTER("연성 연금술사"), SHOOTER("슈터"), NINJA("닌자"), CHAIN_SLASHER("체인 슬래셔"),
-  BATTLE_ASTROLOGER("전투 점성술사"), ADVENTURER("모험가"), MERCHANT("상인"), PRIEST("사제"),
+  ALCHEMY_MASTER("연성연금술사"), SHOOTER("슈터"), NINJA("닌자"), CHAIN_SLASHER("체인슬래셔"),
+  BATTLE_ASTROLOGER("전투점성술사"), ADVENTURER("모험가"), MERCHANT("상인"), PRIEST("사제"),
   CHEF("요리사"), BLACKSMITH("대장장이"), TAILOR("재단사"), PHARMACIST("약사"),
-  CARPENTER("목수"), PET_HANDLER("펫 핸들러"), MAGIGRAPHER("마기그래퍼"), DESTINY_ASTROLOGER("운명 점성술사"),
-  MERLIN("멀린"), DIVA("디바"), PROFESSOR_J("프로페서 J"), MASTER_CHEF("마스터셰프"),
-  TREASURE_HUNTER("트레저 헌터");
+  CARPENTER("목수"), PET_HANDLER("펫핸들러"), MAGIGRAPHER("마기그래퍼"), DESTINY_ASTROLOGER("운명점성술사"),
+  MERLIN("멀린"), DIVA("디바"), PROFESSOR_J("프로페서J"), MASTER_CHEF("마스터셰프"),
+  TREASURE_HUNTER("트레저헌터");
 
   private final String value;
   private static final Random RANDOM = new Random();
@@ -31,6 +32,8 @@ public enum NicknameWord {
   }
 
   public static boolean contains(String value) {
-    return Arrays.stream(values()).anyMatch(v -> v.getValue().equals(value));
+    return Arrays.stream(values())
+      .anyMatch(v -> v.getValue()
+        .equals(value));
   }
 }
